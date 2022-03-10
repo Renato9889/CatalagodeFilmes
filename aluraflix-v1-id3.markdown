da8bbad28e0a0eddf5d6eb185265febc0499f575
Renato9889 <38532053+Renato9889@users.noreply.github.com>
2022-03-10 20:52:21 -0300

    Add files via upload

---
+++ aluraflix-v1-id3.markdown
@@ -0,0 +1,7 @@
+AluraFlix v1- ID3
+-----------------
+
+
+A [Pen](https://codepen.io/renato9889/pen/WNXVBvp) by [Renato9889](https://codepen.io/renato9889) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/WNXVBvp).
---
+++ script.js
@@ -0,0 +1,13 @@
+var listaFilmes = [
+  "https://upload.wikimedia.org/wikipedia/pt/f/f8/X-Men_Dias_Futuro_Esquecido_2014.jpg",
+  "https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg",
+  "https://cdn.awsli.com.br/600x700/1610/1610163/produto/92159380/poster-liga-da-justica-zack-snyder-b-a9b1b8fa.jpg",
+  "https://br.web.img3.acsta.net/pictures/19/09/17/19/29/5316438.jpg",
+  "https://img.elo7.com.br/product/original/3BC773A/quadro-com-vidro-geek-vingadores-guerra-infinita-tamanho-a4-friends-moldura.jpg",
+  "https://br.web.img3.acsta.net/pictures/22/03/02/19/26/3666027.jpg",
+  "https://br.web.img3.acsta.net/pictures/22/02/14/18/29/1382589.png"
+];
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
